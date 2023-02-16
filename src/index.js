ad38f2832f5150e727379ab46b39fdb7e76e8aa7
Natasha3x3 <npostekova@gmail.com>
2023-02-16 17:51:36 +0100

    Added current date

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        `0${hours}`
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        `0${minutes}`
+    }
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+ return `Last updated: ${day} ${hours}:${minutes}`;   
+}
 
 let apiKey = "f3fcd99b4efc8a3098994617c9de63f0";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Bratislava&appid=${apiKey}&units=metric`;
@@ -13,6 +27,10 @@ let humidity = document.querySelector("#humidity");
 humidity.innerHTML =`Humidity: ${response.data.main.humidity}%`;
 let wind = document.querySelector("#wind");
 wind.innerHTML = `Wind speed: ${Math.round(response.data.wind.speed)} km/h`;
+let currentDate = document.querySelector("#currentDate");
+currentDate.innerHTML= formatDate(response.data.dt * 1000);
+
 }
+
 
 axios.get(apiUrl).then(displayTemperature);
